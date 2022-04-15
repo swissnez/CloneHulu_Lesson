5bdfa269e0aa86867cca7ac8716126f9378fb1ba
Neal Palmer <neal@abovebeyondtech.se>
2022-04-15 12:41:10 +0200

    first commit for upload

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-const modal = document.querySelector('.modal')
-const loginBtn = document.querySelector('.login-btn')
-const closeBtn = document.querySelector('.close')
+//jshint esversion:9
+const modal = document.querySelector(".modal");
+const loginBtn = document.querySelector(".login-btn");
+const closeBtn = document.querySelector(".close-btn");
 
-loginBtn.addEventListener('click', openModal)
-closeBtn.addEventListener('click', closeModal)
-window.addEventListener('click', outsideClick)
 
-function openModal() {
-  modal.style.display = 'block'
-}
 
-function closeModal() {
-  modal.style.display = 'none'
-}
 
-function outsideClick(e) {
-  if (e.target == modal) {
-    closeModal()
-  }
-}
+
+const openModal = () => modal.style.display = "block";  
+
+
+const closeModal = (e) => modal.style.display = "none";
+
+
+const outside = (e) => e.target == modal ? closeModal() : null;
+// Eventlisteners 
+loginBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", outside);
+
